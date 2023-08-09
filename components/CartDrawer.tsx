8f90756bd37dbf9a6076af5709c7c2b49e30b29c
Sebastian Sala <sebastian_orko@hotmail.com>
2023-08-08 20:01:05 -0600

    cart drawer css

--- components/CartDrawer.tsx
+++ components/CartDrawer.tsx
@@ -34,32 +34,34 @@ const CartDrawer = () => {
       <button onClick={showDrawer}>
         <AiOutlineShoppingCart className='text-2xl' />
       </button>
-      <Drawer
-        placement='right'
-        onClose={closeDrawer}
-        open={isOpen}
-        width='550'
-        className='relative'
-      >
-        {cart.length === 0 ? (
-          <div className='grid place-content-center place-items-center h-full w-full'>
-            <BagIcon />
-            <p className='text-2xl'>Your cart is empty</p>
-          </div>
-        ) : (
-          <ul className='space-y-8'>
-            {sortedCart.map((product) => (
-              <CartProduct product={product} key={product.id} />
-            ))}
-          </ul>
-        )}
-        <footer className='fixed bottom-0 py-10 w-full px-10 bg-white'>
-          <div className='flex justify-between items-center'>
-            <span>Subtotal:</span>
-            <span>$ {total}</span>
-          </div>
-          <button></button>
-        </footer>
+      <Drawer placement='right' onClose={closeDrawer} open={isOpen} width='550'>
+        <div className='h-full relative w-full'>
+          {cart.length === 0 ? (
+            <div className='grid place-content-center place-items-center h-full w-full'>
+              <BagIcon />
+              <p className='text-2xl'>Your cart is empty</p>
+            </div>
+          ) : (
+            <ul className='h-5/6 space-y-8 overflow-y-auto flex flex-col'>
+              {sortedCart.map((product) => (
+                <CartProduct product={product} key={product.id} />
+              ))}
+            </ul>
+          )}
+          <footer className='absolute w-full bottom-0 mt-auto py-10 bg-white'>
+            <div className='flex items-center w-full gap-4'>
+              <div>
+                <span>Subtotal:</span>
+                <span>$ {total}</span>
+              </div>
+              <div className='flex-1'>
+                <button className='px-2 py-4 bg-orange-400 text-white w-full rounded'>
+                  Checkout
+                </button>
+              </div>
+            </div>
+          </footer>
+        </div>
       </Drawer>
     </>
   )
